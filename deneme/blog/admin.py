0e31eed939cafcc060e3b0d7a9d8e56f10b5d7dd
Ifmai <94466351+Ifmai@users.noreply.github.com>
2024-07-07 00:19:03 +0300

    admin model eklendi admin page düzenlendi ve slugfield kullanımı öğrenildi.

--- deneme/blog/admin.py
+++ deneme/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Blog, Catagory
+
+
+class BlogActiveList(admin.ModelAdmin):
+	list_display = ("title", "is_home", "is_active", "slug")
+	list_editable = ("is_home", "is_active")
+	search_fields = ("title", "context")
+	readonly_fields = ("title","image", "slug") # sadece readonly yapıyor ve admin page de değiştirilmiyor.
 
 # Register your models here.
+admin.site.register(Blog, BlogActiveList)
+admin.site.register(Catagory)
--- deneme/blog/models.py
+++ deneme/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Blog(models.Model):
@@ -7,8 +8,17 @@ class Blog(models.Model):
 	context = models.TextField(max) # defaul değer 255 dir.
 	is_active = models.BooleanField()
 	is_home = models.BooleanField(default=False) # bir bilgi kaydedildiğinde default değer false eklenir. veri kaydederken göndermek zorunda kalmayız.
-
+	slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+	def __str__(self) -> str:
+		return f"{self.title}"
+	
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super().save(*args, **kwargs)
 
 class Catagory(models.Model):
 	name = models.CharField(max_length=150)
+
+	def __str__(self) -> str:
+		return f"{self.name}"
 
