6ac615b06b2545a7f2fe1f151c276c786ad8791f
IremOztimur <iremoztimur07@gmail.com>
2024-09-16 17:31:39 +0300

    [CREATE] matchmaking router

---
+++ srcs/matchmaking/api/routing.py
@@ -0,0 +1,7 @@
+# routing.py
+from django.urls import re_path
+from .consumers import MatchMakerConsumer
+
+websocket_urlpatterns = [
+    re_path(r'ws/matchmaking/$', MatchMakerConsumer.as_asgi()),
+]
