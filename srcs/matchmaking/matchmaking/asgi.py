b2b3b75220fc9f6c8246af9fd0f8f232160a6342
IremOztimur <iremoztimur07@gmail.com>
2024-09-16 17:34:45 +0300

    [ADD] websocket_urlpatterns added and used AuthMiddlewareStack

--- srcs/matchmaking/matchmaking/asgi.py
+++ srcs/matchmaking/matchmaking/asgi.py
@@ -10,11 +10,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from api.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matchmaking.settings')
 
 application = ProtocolTypeRouter(
 	{
 		'http': get_asgi_application(),
+		'websocket' : AuthMiddlewareStack(
+			URLRouter(
+				websocket_urlpatterns
+			)
+		),
 	})
