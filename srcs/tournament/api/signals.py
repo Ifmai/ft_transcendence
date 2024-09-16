776a5ce24976cf00066c1a08af68e6c544160e3a
ifmai <alpozdemir044@gmail.com>
2024-09-16 15:13:13 +0000

    [ADD] signals file added for the tournament service

---
+++ srcs/tournament/api/signals.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from api.models import Profil, ProfileComment
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+#Alıcı(receiver) User da bir kaydetme işleminden sonra çalışacak.
+@receiver(post_save, sender=User)
+def create_profil(sender, instance, created, **kwargs):
+	print(instance.username , '__Created: ', created )
+	if created:
+		Profil.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Profil)
+def create_first_durum(sender, instance, created, **kwargs):
+	print(instance.user.username , '__Created: ', created )
+	if created:
+		ProfileComment.objects.create(
+			user_profil = instance,
+			comment_text = f'{instance.user.username} attended the last dance ball..',
+		)
