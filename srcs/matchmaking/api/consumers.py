c60cb52bdf5bbe5f568cdf7369fb8be5125fc414
IremOztimur <iremoztimur07@gmail.com>
2024-09-17 18:10:49 +0300

    [ADD] MatchMakerConsumer implemented with connect, recieve methods

--- srcs/matchmaking/api/consumers.py
+++ srcs/matchmaking/api/consumers.py
@@ -1,7 +1,24 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.layers import get_channel_layer
+import uuid
+#from .models import Match
 
-class MatchMakerConsumer(WebsocketConsumer):
-	def connect(self):
-		self.accept()
-		self.send(text_data=json.dumps({'message': 'Connected', 'status': 200}))
+class MatchMakerConsumer(AsyncWebsocketConsumer):
+	async def receive(self, text_data):
+		print("text_data: ", text_data)
+		await self.send(text_data=json.dumps({'message': 'Received', 'data': text_data}))
+
+	async def connect(self):
+		try:
+			match_id = self.scope['url_route']['kwargs'].get('match_id')
+			capacity =  self.scope['url_route']['kwargs'].get('capacity')
+		except KeyError as e:
+			print(f"Error getting scope: {e}")
+
+		await self.accept()
+		await self.send(text_data=json.dumps({'message': 'Connected', 'status': 200, 'match_id': match_id}))
+
+	async def disconnect(self, close_code):
+		pass
+
--- srcs/matchmaking/api/routing.py
+++ srcs/matchmaking/api/routing.py
@@ -1,7 +1,9 @@
 # routing.py
-from django.urls import re_path
+from django.urls import path, re_path
 from .consumers import MatchMakerConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/matchmaking/$', MatchMakerConsumer.as_asgi()),
+	re_path(r'ws/matchmaking/$', MatchMakerConsumer.as_asgi()),
+	path('ws/matchmaking/<int:capacity>/<int:match_id>/', MatchMakerConsumer.as_asgi()),
+	path('ws/matchmaking/<int:capacity>/', MatchMakerConsumer.as_asgi()),
 ]
