247e62173cdbf849e777104e8dcb76cdd59e2269
ifmai <alpozdemir044@gmail.com>
2024-09-24 15:56:27 +0000

    [UPDATE] turnuva oluşturulduğunda turnuva listesi artık tekrar yenilenip sayfa load yapılmadan veriler işleniyor.

--- srcs/nginx/htmlfile/js/tournament.js
+++ srcs/nginx/htmlfile/js/tournament.js
@@ -1,5 +1,28 @@
 const tournaments = [];
 
+async function add_tournaments(){
+	const tournamentList = document.getElementById('trn-tournamentList');
+	tournaments.forEach(tournament => {
+		const tournamentItem = document.createElement('div');
+		tournamentItem.classList.add('trn-tournament-item');
+		tournamentItem.innerHTML = `
+			<h2 class="trn-tournament-name">${tournament.name}</h2>
+			<p class="trn-tournament-creator">Created by: ${tournament.creator}</p>
+			<a href="#" class="trn-button" id='${tournament.id}'>Join Tournament</a>
+		`;
+		tournamentList.appendChild(tournamentItem);
+	});
+	tournamentList.addEventListener('click', (e) => {
+		if (e.target.classList.contains('trn-button')) {
+			console.log("tıklanan buton id : ", e.target.id);
+			e.preventDefault();
+			const tournamentName = e.target.closest('.trn-tournament-item').querySelector('.trn-tournament-name').textContent;
+			document.getElementById('trn-joinPopupOverlay').style.display = 'flex';
+			document.querySelector('#trn-joinPopupOverlay .trn-popup-title').textContent = `Join Tournament: ${tournamentName}`;
+		}
+	});
+}
+
 async function get_tournaments_list(){
 	try {
 		const response = await fetch('/api/tournament/get/', {
@@ -9,14 +32,18 @@ async function get_tournaments_list(){
 				'Authorization': `Bearer ${getCookie('access_token')}`
 			}
 		});
-		data = await response.json();
-		console.log("data : ", data);
-        const formattedTournaments = data.map(item => ({
-			id : item.tournaments_id,
-            name: item.name,
-            creator: item.creator_user
-        }));
-        tournaments.push(...formattedTournaments);
+		if(response.ok){
+			data = await response.json();
+			console.log("data : ", data);
+			const formattedTournaments = data.map(item => ({
+				id : item.tournaments_id,
+				name: item.name,
+				creator: item.creator_user
+			}));
+			tournaments.push(...formattedTournaments);
+			await add_tournaments();
+		}else
+			throw new Error(`HTTP error! Status: ${response.status}`);
 	} catch (error) {
 		console.error(error);
 	}
@@ -47,32 +74,9 @@ async function create_tournament(tournamentname, nickname){
 	}
 }
 
-async function add_tournaments(){
-	const tournamentList = document.getElementById('trn-tournamentList');
-	tournaments.forEach(tournament => {
-		const tournamentItem = document.createElement('div');
-		tournamentItem.classList.add('trn-tournament-item');
-		tournamentItem.innerHTML = `
-			<h2 class="trn-tournament-name">${tournament.name}</h2>
-			<p class="trn-tournament-creator">Created by: ${tournament.creator}</p>
-			<a href="#" class="trn-button" id='${tournament.id}'>Join Tournament</a>
-		`;
-		tournamentList.appendChild(tournamentItem);
-	});
-	tournamentList.addEventListener('click', (e) => {
-		if (e.target.classList.contains('trn-button')) {
-			console.log("tıklanan buton id : ", e.target.id);
-			e.preventDefault();
-			const tournamentName = e.target.closest('.trn-tournament-item').querySelector('.trn-tournament-name').textContent;
-			document.getElementById('trn-joinPopupOverlay').style.display = 'flex';
-			document.querySelector('#trn-joinPopupOverlay .trn-popup-title').textContent = `Join Tournament: ${tournamentName}`;
-		}
-	});
-}
-
 async function tournamentPage() {
 	await get_tournaments_list();
-	await add_tournaments();
+	
 	// Create Tournament button functionality
 	document.getElementById('trn-createTournamentBtn').addEventListener('click', (e) => {
 		e.preventDefault();
@@ -82,7 +86,7 @@ async function tournamentPage() {
 	// Join Tournament button functionality
 
 	// Join Game button functionality
-	document.getElementById('trn-joinButton').addEventListener('click', () => {
+	document.getElementById('trn-joinButton').addEventListener('click', async () => {
 		const nickname = document.getElementById('trn-joinNicknameInput').value;
 		if (nickname) {
 			alert(`Joining tournament with nickname: ${nickname}`);
@@ -100,6 +104,7 @@ async function tournamentPage() {
 		console.log("ismi : ", tournamentName, " nickname : ", nickname);
 		if (tournamentName && nickname) {
 			await create_tournament(tournamentName, nickname);
+			await get_tournaments_list();
 			document.getElementById('trn-createPopupOverlay').style.display = 'none';
 			document.getElementById('trn-createTournamentInput').value = '';
 			document.getElementById('trn-createNicknameInput').value = '';
