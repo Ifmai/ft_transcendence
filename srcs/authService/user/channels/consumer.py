03fc84e5c109565888f35b748b2a66cb8a639c5f
Ifmai <alpozdemir044@gmail.com>
2024-09-28 21:53:52 +0000

    [ADD] arkadaşlık istekleri(accept/reject), arkadaşların chat sayfasına yüklenmesi vb. işlemlerin hepsi soket üzerine taşındı anlık değişmesi için.

--- srcs/authService/user/channels/consumer.py
+++ srcs/authService/user/channels/consumer.py
@@ -3,13 +3,14 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from user.models import Profil, UserFriendsList
 from django.contrib.auth.models import User
 from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 
 import json
 
 class FriendListConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
-		print("self scope : ", self.scope['user'])
 		if self.scope['user'].is_authenticated:
 			self.user = self.scope['user']
 			self.room_group_name = f"friend_list_{self.user.username}"
@@ -20,16 +21,134 @@ class FriendListConsumer(AsyncWebsocketConsumer):
             	self.channel_name
 			)
 			await self.accept()
-			await self.notify_friends('online')
-	
+			await self.notify_friends('ON')
+
+	async def receive(self, text_data):
+		text_data_json = json.loads(text_data)
+		m_type = text_data_json["type"]
+		if m_type == 'list_request':
+			await self.list_request()
+		elif m_type == 'friend_request':
+			r_username = text_data_json['name']
+			await self.friend_Request(r_username)
+		elif m_type == 'friend_request_response':
+			s_username = text_data_json['username']
+			r_response = text_data_json['response']
+			await self.friend_request_response(s_username, r_response)
+		elif m_type == 'friend_request_list':
+			await self.friend_request_list()
+
 	async def disconnect(self, code):
 		pass
 		if self.user:
 			await self.update_online_status('OF')
-			await self.notify_friends('offline')
+			await self.notify_friends('OF')
 			await self.channel_layer.group_discard(
 				self.room_group_name,
 				self.channel_name
+			)
+
+
+	#Friends Request Response Accept/Reject
+
+	@database_sync_to_async
+	def friend_request_update(self, r_response, sender, receiver):
+		try:
+			f_request = UserFriendsList.objects.get(sender=sender, receiver=receiver, friend_request=False)
+
+			if r_response == 'accept':
+				f_request.friend_request = True
+				f_request.save()
+				return "accepted"
+			
+			elif r_response == 'reject':
+				f_request.delete()
+				return "rejected"
+
+		except ObjectDoesNotExist:
+			return "Friend Request Not Found"
+
+	async def friend_request_response(self, s_username, r_response):
+		s_user = await self.get_user(s_username)
+		s_msg = await self.friend_request_update(r_response, s_user, self.user)
+		await self.send(text_data=json.dumps({
+			'type' : 'friend_request_response',
+			'Response': s_msg
+		}))
+
+
+	#Friends Request List
+	@database_sync_to_async
+	def check_is_friend_request(self, sender, receiver):
+		both_friends = UserFriendsList.objects.filter(
+        	(Q(sender=sender) & Q(receiver=receiver)) | (Q(sender=receiver) & Q(receiver=sender))
+    	).exists()
+		return both_friends
+
+	@database_sync_to_async
+	def get_request_list(self, user):
+		requests = UserFriendsList.objects.filter(receiver=user , friend_request=False)
+		request_list = []
+		for req in requests:
+			sender_user = req.sender
+			sender_profil = Profil.objects.get(user=sender_user)
+			request_list.append({
+				'username': sender_user.username,
+				'photo': sender_profil.photo.url
+			})
+		print("Request List : ", request_list)
+		return request_list
+
+	async def friend_request_list(self):
+		request_list = await self.get_request_list(self.user)
+		for req in request_list:
+			await self.send(
+				text_data=json.dumps({
+					"type": 'request_list',
+					"user" : req['username'],
+					"photo": req['photo']
+				})
+			)
+	#Friends Request List END
+
+
+	#Friend Request 
+	@database_sync_to_async
+	def add_friends_list(self, sender, receiver):
+		friend = UserFriendsList.objects.create(sender=sender, receiver=receiver)
+		if friend:
+			return True
+		else:
+			return False
+	
+	async def friend_Request(self, r_username):
+		receiver = await self.get_user(r_username)
+		if receiver:
+			sender = self.user
+			if receiver == sender:
+				await self.send(text_data=json.dumps({'error' : 'kendine arkadaşlık isteği atamazsın.'}))
+			elif await self.check_is_friend_request(sender, receiver):
+				await self.send(text_data=json.dumps({'error' : 'Sen zaten istke atmışsın bro.'}))
+			else:
+				if await self.add_friends_list(sender, receiver):
+					await self.send(text_data=json.dumps({'Succses' : 'Friend Request Send'}))
+				else:
+					await self.send(text_data=json.dumps({'Error' : 'Server error.'}))
+	#Friend Request END
+
+
+	#Friends online/ofline && Friends List Functions START
+
+	async def list_request(self):
+		friend_list = await self.get_friends()
+		for friend in friend_list:
+			await self.send(
+				text_data=json.dumps({
+					"type": 'activity',
+					"user" : friend['username'],
+					"status": friend['is_online'],
+					"photo": friend['photo']
+				})
 			)
 
 	async def notify_friends(self, status):
@@ -74,6 +193,16 @@ class FriendListConsumer(AsyncWebsocketConsumer):
 			friends_data.append({
 				'id': friend_user.id,
 				'username': friend_user.username,
-				'is_online': status
+				'is_online': status,
+				'photo': friend_profile.photo.url
 			})
 		return friends_data
+	#Friends online/ofline && Friends List Functions END
+
+	@database_sync_to_async
+	def get_user(self, username):
+		r_user = User.objects.get(username=username)
+		if r_user:
+			return r_user
+		else:
+			return False
--- srcs/nginx/htmlfile/js/friends.js
+++ srcs/nginx/htmlfile/js/friends.js
@@ -1,110 +1,185 @@
 //friends js başlangıç.
 //let friendRequests = null;
-// const friends = [
-//     { name: "Flynn", avatar: "/placeholder.svg?height=40&width=40" },
-//     { name: "Quorra", avatar: "/placeholder.svg?height=40&width=40" },
-//     { name: "Tron", avatar: "/placeholder.svg?height=40&width=40" },
-//     { name: "Clu", avatar: "/placeholder.svg?height=40&width=40" },
-//     { name: "Sam", avatar: "/placeholder.svg?height=40&width=40" }
-// ];
+let friends = [
+];
 
-// // Populate friend list
-// const friendList = document.getElementById('friendList');
-// friends.forEach(friend => {
-//     const friendElement = document.createElement('div');
-//     friendElement.className = 'chat-friend';
-//     friendElement.innerHTML = `
-//                 <img src="${friend.avatar}" alt="${friend.name}" class="chat-friend-avatar">
-//                 <span class="chat-friend-name">${friend.name}</span>
-//             `;
-//     friendList.insertBefore(friendElement, friendList.lastElementChild);
-// });
+let pendingRequests = [
+];
 
-// const friendRequestButton = document.getElementById('friendRequestButton');
-// const friendRequestPopup = document.getElementById('friendRequestPopup');
-// const overlay = document.getElementById('overlay');
-// friendRequests = document.getElementById('friendRequests');
-// const newFriendInput = document.getElementById('newFriendInput');
-// const sendFriendRequestButton = document.getElementById('sendFriendRequestButton');
+async function populateFriendList(friend) {
+    const friendList = document.getElementById('friendList');
 
-// friendRequestButton.addEventListener('click', () => {
-// 	friendRequestPopup.style.display = 'block';
-// 	overlay.style.display = 'block';
-// });
+    const existingFriend = friendList.querySelector(`#${friend.username}`);
+    if (existingFriend) {
+        return;
+    }
 
-// overlay.addEventListener('click', () => {
-// 	friendRequestPopup.style.display = 'none';
-// 	overlay.style.display = 'none';
-// });
+    const friendElement = document.createElement('div');
+    friendElement.className = 'chat-friend';
+    friendElement.id = friend.username;
+    friendElement.innerHTML = `
+            <img src="${friend.photo}" alt="${friend.username}" class="chat-friend-avatar">
+            <span class="chat-friend-name">${friend.username}</span>
+            <div class="chat-friend-status ${friend.status}" id="${friend.username}"></div>
+        `;
+    friendList.insertBefore(friendElement, friendList.lastElementChild);
+}
 
-// // Sample friend request data
-// const pendingRequests = [
-// 	{ name: "Rinzler", avatar: "/placeholder.svg?height=40&width=40" },
-// 	{ name: "Zuse", avatar: "/placeholder.svg?height=40&width=40" },
-// 	{ name: "Gem", avatar: "/placeholder.svg?height=40&width=40" }
-// ];
+function addFriendRequest(request) {
+    const friendRequests = document.getElementById('friendRequests');
 
-// // Populate friend request list
+    const existingFriend = friendRequests.querySelector(`#${request.username}`);
+    if (existingFriend) {
+        return;
+    }
 
-// pendingRequests.forEach(addFriendRequest);
+    const requestElement = document.createElement('div');
+    requestElement.className = 'chat-friend-request';
+    requestElement.innerHTML = `
+                <img src="${request.photo}" alt="${request.username}" class="chat-friend-request-avatar">
+                <div class="chat-friend-request-info">
+                    <div class="chat-friend-request-name">${request.username}</div>
+                    <div class="chat-friend-request-actions">
+                        <button class="chat-friend-request-accept" id="${request.username}">Accept</button>
+                        <button class="chat-friend-request-reject" id="${request.username}">Reject</button>
+                    </div>
+                </div>
+            `;
+    friendRequests.appendChild(requestElement);
+}
 
-// // Handle friend request actions
-// friendRequests.addEventListener('click', (e) => {
-// 	if (e.target.classList.contains('chat-friend-request-accept') || e.target.classList.contains('chat-friend-request-reject')) {
-// 		const requestElement = e.target.closest('.chat-friend-request');
-// 		requestElement.remove();
-// 	}
-// });
+async function friendList() {
+    const friendRequests = document.getElementById('friendRequests');
+    const sendFriendRequestButton = document.getElementById('sendFriendRequestButton');
+    const friendRequestButton = document.getElementById('friendRequestButton');
+    const friendRequestPopup = document.getElementById('friendRequestPopup');
+    const newFriendInput = document.getElementById('newFriendInput');
+    const overlay = document.getElementById('overlay');
 
-// // Send new friend request
-// sendFriendRequestButton.addEventListener('click', () => {
-// 	const newFriendName = newFriendInput.value.trim();
-// 	if (newFriendName) {
-// 		addMessage('System', `Friend request sent to ${newFriendName}`, '/placeholder.svg?height=40&width=40');
-// 		newFriendInput.value = '';
-// 		friendRequestPopup.style.display = 'none';
-// 		overlay.style.display = 'none';
-// 	}
-// });
+    ws.send(JSON.stringify({ 
+        'type' : 'list_request',
+    }));
 
-// newFriendInput.addEventListener('keypress', (e) => {
-// 	if (e.key === 'Enter') {
-// 		sendFriendRequestButton.click();
-// 	}
-// });
+    friendRequests.addEventListener('click', (e) => {
+        if (e.target.classList.contains('chat-friend-request-accept') || e.target.classList.contains('chat-friend-request-reject')) {
+            const friendId = e.target.id;
+            let response;
+            if(e.target.classList.contains('chat-friend-request-accept'))
+                response = 'accept';
+            else
+                response = 'reject';
+            ws.send(JSON.stringify({
+                'type' : 'friend_request_response',
+                'username': friendId,
+                'response' : response
+            }));
+            const requestElement = e.target.closest('.chat-friend-request');
+            requestElement.remove();
+        }
+    });
 
-// function addFriendRequest(request) {
-//     const requestElement = document.createElement('div');
-//     requestElement.className = 'chat-friend-request';
-//     requestElement.innerHTML = `
-//                 <img src="${request.avatar}" alt="${request.name}" class="chat-friend-request-avatar">
-//                 <div class="chat-friend-request-info">
-//                     <div class="chat-friend-request-name">${request.name}</div>
-//                     <div class="chat-friend-request-actions">
-//                         <button class="chat-friend-request-accept">Accept</button>
-//                         <button class="chat-friend-request-reject">Reject</button>
-//                     </div>
-//                 </div>
-//             `;
-//     friendRequests.appendChild(requestElement);
-// }
+    newFriendInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            sendFriendRequestButton.click();
+        }
+    });
+
+    sendFriendRequestButton.addEventListener('click', () => {
+        const newFriendName = newFriendInput.value.trim();
+        if (newFriendName) {
+            addMessage('System', `Friend request sent to ${newFriendName}`, '/placeholder.svg?height=40&width=40');
+            ws.send(JSON.stringify({
+                'type': 'friend_request',
+                'name': newFriendName
+            }));
+            newFriendInput.value = '';
+        }
+    });
+
+    friendRequestButton.addEventListener('click', () => {
+        friendRequestPopup.style.display = 'block';
+        overlay.style.display = 'block';
+        ws.send(JSON.stringify({
+            'type' : 'friend_request_list'
+        }));
+    });
+    overlay.addEventListener('click', () => {
+        friendRequestPopup.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+}
 
 
+async function initWebSocket() {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+        console.log('WebSocket zaten açık.');
+        return;
+    }
+    token = getCookie('access_token')
+    if(!token) {
+        console.log('Kullanıcı oturum açmamış, WebSocket bağlantısı oluşturulmadı.');
+        return;
+    }
 
-// async function friendList() {
-// 	initWebSocket2();
-// 	const friendsBtn = document.getElementById('addFriends');
-// 	const requestsList = document.getElementById('requests-list');
+    // Yeni WebSocket bağlantısı oluştur
+    ws = new WebSocket(`wss://lastdance.com.tr/ws/friend-list/?token=${getCookie('access_token')}`);
+    ws.onopen = function(event) {
+        console.log('WebSocket bağlantısı açıldı.');
+    };
 
-// 	await friends_request_list()
-// 	await user_friends_list()
-// 	friendsBtn.addEventListener('click', async function(event) {
-// 		event.preventDefault();
-// 		const usernameInput = document.getElementById('input-username').value;
-// 		await add_friends(usernameInput);
-// 	});
-// 	requestsList.addEventListener('click', function(event) {
-// 		event.preventDefault();
-// 		friends_accept(event);
-// 	});
-// }
+    ws.onmessage = async function(event) {
+        const data = JSON.parse(event.data);
+        if(data['type'] == 'activity'){
+            friends = []
+            const friend = {
+                'username': data['user'],
+                'photo': data['photo'],
+                'status': data['status']
+            }
+            friends.push(friend);
+            populateFriendList(friend);
+        }
+        else if (data['type'] == 'friend_status'){
+            const statusDiv = document.getElementById(data['username']);
+            const newStatus = data['status']
+            if (statusDiv) {
+                statusDiv.className = newStatus === 'ON' ? 'chat-friend-status ON' : 'chat-friend-status OF';
+            }
+        }
+        else if(data['type'] == 'request_list'){
+            pendingRequests = []
+            const friend = {
+                'username': data['user'],
+                'photo': data['photo']
+            }
+            pendingRequests.push(friend);
+            addFriendRequest(friend);
+        }
+        else if(data['type'] == 'friend_request_response'){
+            if(data['Response'] == 'accepted'){
+                ws.send(JSON.stringify({ 
+                    'type' : 'list_request',
+                }));
+            }
+            ws.send(JSON.stringify({ 
+                'type' : 'friend_request_list',
+            }));
+        }
+    };
+
+    ws.onclose = function(event) {
+        console.log('WebSocket bağlantısı kapandı.');
+    };
+
+    ws.onerror = function(event) {
+        console.error('WebSocket hata:', event);
+    };
+}
+
+//WebSocket bağlantısını kapatma fonksiyonu
+function closeWebSocket() {
+    if (ws) {
+        ws.close();
+        ws = null;
+    }
+}
