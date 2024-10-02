adfa288f2201eae6b6b70edebee20441de636f51
IremOztimur <iremoztimur07@gmail.com>
2024-10-02 19:40:40 +0300

    [ADD] paddle class implemented

---
+++ srcs/game/api/templates/js/components/paddle.js
@@ -0,0 +1,28 @@
+class Paddle {
+    constructor(x, y, width, height, dy) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.dy = dy;
+    }
+
+    update() {
+        this.y += this.dy;
+        if (this.y < 0) {
+            this.y = 0;
+        } else if (this.y + this.height > window.innerHeight) {
+            this.y = window.innerHeight - this.height;
+        }
+    }
+
+    updateState(data) {
+        this.y = data['positionY'];
+        this.dy = data['velocity'];
+    }
+
+    draw(context) {
+		context.fillStyle = "#33ff00";
+		context.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
