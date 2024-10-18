a6cb36952db21bd99b3723063d8c41bca3d584ea
Ahmet Salih Temel <ahmtemel@k1m17s05.42istanbul.com.tr>
2024-10-19 00:38:03 +0300

    pop up added for succesfully register ,different password  and doesnt match with password rules. Changed leftscore with rightscore

--- srcs/nginx/htmlfile/js/register.js
+++ srcs/nginx/htmlfile/js/register.js
@@ -1,3 +1,16 @@
+function showPopup(message, success = false) {
+    const popup = document.getElementById('popup-message');
+    popup.textContent = message;
+
+    // Başarılı ise yeşil renk, hata ise kırmızı renk
+    popup.style.backgroundColor = success ? 'rgba(72, 187, 120, 0.9)' : 'rgba(220, 38, 38, 0.9)';
+
+    popup.style.display = 'block';
+
+    setTimeout(() => {
+        popup.style.display = 'none';
+    }, 4000);
+}
 
 async function registerPage(){
     const registerBtn = document.getElementById('registerBtn');
@@ -10,7 +23,9 @@ async function registerPage(){
         const pass2 = document.getElementById('register-confirm-password').value;
 
         if(pass1 !== pass2)
-            alert("Şifreler aynı değil.");
+        {
+            showPopup('Şifreler Aynı Değil');
+        }
         else{
             try {
                 const response = await fetch('https://lastdance.com.tr/api/users/register/', {
@@ -29,8 +44,17 @@ async function registerPage(){
                 console.log('Status Code:', response.status);
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
-                }else
+
+                }
+                else if(response.status === 400)
+                {
+                    showPopup('Lütfen kurallara uygun bir şifre belirleyin');
+                }
+                else
+                {
+                    showPopup('Başarılı bir şekilde kayıt yapıldı!', true);
                     loadPage(selectPage('/'));
+                }
             } catch (error) {
                 console.error('Error:', error);
             }
